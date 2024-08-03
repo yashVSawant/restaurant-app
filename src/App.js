395a31f519e69463e806ce27c240f3670a7e186a
yashVSawant <yashsawant23910@gmail.com>
2024-08-03 14:44:44 +0530

    add summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Summary from "./components/Meals/Summary";
 
 function App() {
   return (
     <div>
       <Header/>
+      <Summary/>
     </div>
   );
 }
